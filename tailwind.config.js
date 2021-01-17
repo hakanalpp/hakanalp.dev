f04cfd4659fc5de68ff373a7ac53d0bb6ec8dc3a
hakanalp <hakana926@gmail.com>
2021-01-17 20:23:06 +0300

    new color scheme

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,12 @@ module.exports = {
     extend: {},
     fontFamily: {
       sans: ['Varela Round', 'sans-serif']
+    },
+    colors: {
+      'dark-green': '#164A41',
+      'light-green': '#9DC88D',
+      'light-yellow': '#F1B24A',
+      'neutral-white': '#E9E9ED'
     }
   },
   variants: {
