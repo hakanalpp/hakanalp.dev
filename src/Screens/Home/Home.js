d82ecf95559614f324ccea5bc22ca9f538e3c5eb
hakanalp <hakana926@gmail.com>
2021-01-19 10:56:22 +0300

    new UI design

--- src/Screens/Home/Home.js
+++ src/Screens/Home/Home.js
@@ -1,23 +1,35 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import './Home.css';
 import { ReactComponent as GitHubLogo } from '../../Assets/svg/github-fa.svg';
 import { ReactComponent as LinkedInLogo } from '../../Assets/svg/linkedin-fa.svg';
+import { ReactComponent as MailLogo } from '../../Assets/svg/mail.svg';
 import photo from '../../Assets/image/my_photo.jpeg';
 
 function Home() {
+  const contactRef = useRef();
+
+  const scrollTo = (ref) => {
+    window.scroll({
+      top: ref.current.offsetTop,
+      behavior: 'smooth'
+    });
+  };
+
   const [isShown0, setIsShown0] = useState(false);
   const [isShown1, setIsShown1] = useState(false);
   const [isShown2, setIsShown2] = useState(false);
+  const [isShown3, setIsShown3] = useState(false);
+  const [isShown4, setIsShown4] = useState(false);
 
   return (
-    <div className="h-screen bg-neutral-white">
-      <div className="px-2 lg:px-0 sm:px-3 h-14 bg-dark-green flex items-center justify-center">
-        <div className="mx-auto flex flex-row justify-between	" style={{ width: '64rem' }}>
+    <div className="bg-gray-700">
+      <div className="px-2 lg:px-0 sm:px-3 bg-gray-700 flex justify-center">
+        <div className="mx-auto flex flex-row justify-between	mt-2" style={{ width: '64rem' }}>
           <button
             className={
               isShown0
-                ? 'text-3xl focus:outline-none text-light-green'
-                : 'text-3xl focus:outline-none text-neutral-white'
+                ? 'text-xl focus:outline-none text-yellow-400'
+                : 'text-xl focus:outline-none text-white'
             }
             onMouseEnter={() => setIsShown0(true)}
             onMouseLeave={() => setIsShown0(false)}>
@@ -30,34 +42,48 @@ function Home() {
               className="focus:outline-none"
               onMouseEnter={() => setIsShown1(true)}
               onMouseLeave={() => setIsShown1(false)}>
-              <GitHubLogo className="w-8 h-8 ml-auto" fill={isShown1 ? '#9DC88D' : '#E9E9ED'} />
+              <GitHubLogo className="w-6 h-6 ml-auto" fill={isShown1 ? '#FBBF24' : '#E9E9ED'} />
             </a>
             <a
               href="https://github.com/hakanalpp"
               className="focus:outline-none"
               onMouseEnter={() => setIsShown2(true)}
               onMouseLeave={() => setIsShown2(false)}>
-              <LinkedInLogo className="w-8 h-8 ml-6" fill={isShown2 ? '#9DC88D' : '#E9E9ED'} />
+              <LinkedInLogo className="w-6 h-6 ml-3" fill={isShown2 ? '#FBBF24' : '#E9E9ED'} />
             </a>
+            <button
+              onClick={() => scrollTo(contactRef)}
+              className="focus:outline-none h-6"
+              onMouseEnter={() => setIsShown4(true)}
+              onMouseLeave={() => setIsShown4(false)}>
+              <MailLogo className="w-6 h-6 ml-3" fill={isShown4 ? '#FBBF24' : '#E9E9ED'} />
+            </button>
           </div>
         </div>
       </div>
-      <div className="cont bg-light-green pt-20">
-        <img
-          className="relative w-64 h-64 rounded-full border-8 border-neutral-white "
-          src={photo}
-          alt="ph"
-        />
-        <p className="text-4xl text-center text-neutral-white mt-8">Hi! I&apos;m Hakan.</p>
-        <p className="text-4xl text-center text-neutral-white mt-2">a Frontend developer.</p>
+
+      <div className="cont">
+        <button
+          className="focus:outline-none"
+          onMouseEnter={() => setIsShown3(true)}
+          onMouseLeave={() => setIsShown3(false)}>
+          <img
+            className="relative w-56 h-56 rounded-full border-2 border-gray-600"
+            style={{ 'border-color': isShown3 ? '#F59E0B' : '#4B5563' }}
+            src={photo}
+            alt="ph"
+          />
+        </button>
+        <p className="text-3xl text-center text-yellow-500 mt-8">Hi! I&apos;m Hakan.</p>
+        <p className="text-3xl text-center text-yellow-500 mt-2">a Frontend developer.</p>
       </div>
 
       <section>
-        <div className="cont bg-light-green bg-neutral-white pt-6">
-          <p className="text-4xl font-semibold text-dark-green">ABOUT ME</p>
-          <div className="divide-y-2 divide-dark-green m-2 text-center">
-            <div className="mx-auto" style={{ width: '300px' }}></div>
-            <div className="mx-auto" style={{ width: '300px' }}></div>
+        <div className="cont mt-4">
+          <p className="head">ABOUT ME</p>
+          <div className="divider">
+            <div className="mx-auto" style={{ width: '250px' }}></div>
+            <div className="mx-auto" style={{ width: '250px' }}></div>
           </div>
           <div className="body">
             <div className="paragraph">
@@ -85,11 +111,11 @@ function Home() {
       </section>
 
       <section>
-        <div className="cont bg-light-green pb-20 pt-6">
-          <p className="text-4xl font-semibold text-dark-green">PROJECTS</p>
-          <div className="divide-y-2 divide-dark-green m-2 text-center">
-            <div className="mx-auto" style={{ width: '300px' }}></div>
-            <div className="mx-auto" style={{ width: '300px' }}></div>
+        <div className="cont">
+          <p className="head">PROJECTS</p>
+          <div className="divider">
+            <div className="mx-auto" style={{ width: '250px' }}></div>
+            <div className="mx-auto" style={{ width: '250px' }}></div>
           </div>
           <div className="body">
             <div className="paragraph">
@@ -119,38 +145,44 @@ function Home() {
         </div>
       </section>
 
-      <section>
-        <div className="cont bg-neutral-white pb-20 pt-6">
-          <p className="text-4xl font-semibold text-dark-green">CONTACT ME</p>
-          <div className="divide-y-2 divide-dark-green m-2 text-center">
-            <div className="mx-auto" style={{ width: '300px' }}></div>
-            <div className="mx-auto" style={{ width: '300px' }}></div>
+      <section ref={contactRef}>
+        <div className="cont">
+          <p className="head justify-self-center">CONTACT ME</p>
+          <div className="divider">
+            <div className="mx-auto" style={{ width: '250px' }}></div>
+            <div className="mx-auto" style={{ width: '250px' }}></div>
           </div>
-          <div className="body">
-            <div className="paragraph">
-              Phasellus lectus augue, finibus quis felis ac, mattis suscipit erat. Nullam suscipit
-              sem et velit pulvinar, vel fringilla tellus interdum. Aenean rutrum nisi sed pretium
-              ultricies. Ut ante massa, hendrerit eu finibus nec, posuere vel sapien. Donec ut
-              fermentum libero. Vivamus volutpat tincidunt lorem in sollicitudin. Pellentesque sit
-              amet congue tortor, hendrerit semper est. Donec sagittis tempor posuere. Nunc
-              venenatis fringilla neque, id sollicitudin nibh rhoncus et. Sed vel ipsum id nunc
-              mattis ultricies blandit a est.
+          <p className="text-bold text-yellow-500">!!! This part is under construction !!!</p>
+          <div className="inputgap">
+            <div className="divider">
+              <div className="cDivider"></div>
+              <div className="cDivider"></div>
             </div>
-            <div className="paragraph pl-4">
-              Integer ac diam sagittis, ullamcorper nunc eget, mollis dui. Integer id elit in justo
-              finibus imperdiet. Maecenas hendrerit ligula vel sem rhoncus, at finibus nisi
-              vehicula. Maecenas molestie nunc vitae justo viverra venenatis. Proin pellentesque
-              enim vitae lorem faucibus, nec eleifend augue tincidunt. Curabitur eu auctor neque.
-              Morbi ligula dolor, consequat vitae sagittis a, faucibus vitae urna. Duis ullamcorper
-              sapien a iaculis aliquet. Ut eros nunc, eleifend quis sapien sit amet, posuere
-              faucibus magna.
+            <input className="input justify-self-start" placeholder="E-mail" />
+          </div>
+          <div className="inputgap">
+            <div className="divider">
+              <div className="cDivider"></div>
+              <div className="cDivider"></div>
             </div>
+            <input className="input justify-self-start" placeholder="Name" />
+          </div>
+          <div className="inputgap">
+            <div className="divider">
+              <div className="cDivider"></div>
+              <div className="cDivider"></div>
+            </div>
+            <input className="input justify-self-start" placeholder="Message" />
+          </div>
+          <div className="divider pt-14">
+            <div className="cDivider"></div>
+            <div className="cDivider"></div>
           </div>
         </div>
       </section>
 
-      <div className="h-16 bg-dark-green flex items-center grid justify-items-center">
-        <p className="text-neutral-white text-2xl">Copyright © Hakan ALP 2021</p>
+      <div className="h-10 bg-gray-700 flex items-center grid justify-items-center">
+        <p className="text-neutral-white text-sm">Copyright © Hakan ALP 2021</p>
       </div>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,15 +2,13 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'neutral-white': '#E9E9ED'
+      }
+    },
     fontFamily: {
       sans: ['Varela Round', 'sans-serif']
-    },
-    colors: {
-      'dark-green': '#164A41',
-      'light-green': '#9DC88D',
-      'light-yellow': '#F1B24A',
-      'neutral-white': '#E9E9ED'
     }
   },
   variants: {
